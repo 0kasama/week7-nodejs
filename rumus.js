b0ee3360bd0aca8a1f355066d1f5519f9ca35dda
0kasama <okaajyad@gmail.com>
2024-02-22 13:14:04 +0700

    Making Module for The Calculation

---
+++ rumus.js
@@ -0,0 +1,22 @@
+function rumusLuasPersegi(sisi) {
+    return sisi * sisi;
+}
+
+function rumusKelPersegi(sisi) {
+    return 4 * sisi;
+}
+
+function rumusLuasPersegiPjg(panjang, lebar) {
+    return panjang * lebar;
+}
+
+function rumusKelPersegiPjg(panjang, lebar) {
+    return 2 * (panjang + lebar);
+}
+
+module.export = {
+    rumusLuasPersegi,
+    rumusKelPersegi,
+    rumusLuasPersegiPjg,
+    rumusKelPersegiPjg,
+};
